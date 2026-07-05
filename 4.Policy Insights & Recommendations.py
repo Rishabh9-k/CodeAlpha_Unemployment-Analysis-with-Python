3b5e43f3b4030d9f90926d2e4744c415a86cfb1f
RISHABH KUMAR GHRITLAHARE <ghritlaharerishabh9@gmail.com>
2026-07-05 22:30:03 +0530

    Update 4.Policy Insights & Recommendations.py

--- 4.Policy Insights & Recommendations.py
+++ 4.Policy Insights & Recommendations.py
@@ -14,3 +14,29 @@ print(top_states_covid)
 corr = df_clean[['Estimated Unemployment Rate (%)', 'Estimated Employed', 'Estimated Labour Participation Rate (%)']].corr()
 print("\nCorrelation Matrix:")
 print(corr)
+
+
+Code output
+Top 5 states with highest average unemployment overall:
+Region
+Tripura             28.350357
+Haryana             26.283214
+Jharkhand           20.585000
+Bihar               18.918214
+Himachal Pradesh    18.540357
+Name: Estimated Unemployment Rate (%), dtype: float64
+
+Top 5 states with highest average unemployment during COVID lockdown (Apr-Jun 2020):
+Region
+Puducherry    57.700000
+Jharkhand     44.896667
+Haryana       37.693333
+Bihar         36.988333
+Tamil Nadu    31.735000
+Name: Estimated Unemployment Rate (%), dtype: float64
+
+Correlation Matrix:
+                                         Estimated Unemployment Rate (%)  Estimated Employed  Estimated Labour Participation Rate (%)
+Estimated Unemployment Rate (%)                                 1.000000           -0.222876                                 0.002558
+Estimated Employed                                             -0.222876            1.000000                                 0.011300
+Estimated Labour Participation Rate (%)                         0.002558            0.011300                                 1.000000

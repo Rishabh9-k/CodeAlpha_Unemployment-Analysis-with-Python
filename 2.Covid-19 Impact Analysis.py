3d198d998d3cf05634052f8076f0fee9489a4866
RISHABH KUMAR GHRITLAHARE <ghritlaharerishabh9@gmail.com>
2026-07-05 22:24:00 +0530

    Update 2.Covid-19 Impact Analysis.py

--- 2.Covid-19 Impact Analysis.py
+++ 2.Covid-19 Impact Analysis.py
@@ -8,3 +8,18 @@ df_clean['Month'] = df_clean['Date'].dt.month
 print("Date range in dataset:", df_clean['Date'].min(), "to", df_clean['Date'].max())
 print("\nSummary Statistics for Unemployment Rate:")
 print(df_clean['Estimated Unemployment Rate (%)'].describe())
+
+
+Code output
+Date range in dataset: 2019-05-31 00:00:00 to 2020-06-30 00:00:00
+
+Summary Statistics for Unemployment Rate:
+count    740.000000
+mean      11.787946
+std       10.721298
+min        0.000000
+25%        4.657500
+50%        8.350000
+75%       15.887500
+max       76.740000
+Name: Estimated Unemployment Rate (%), dtype: float64

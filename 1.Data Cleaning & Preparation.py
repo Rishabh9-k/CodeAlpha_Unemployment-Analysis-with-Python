cf0876ebb1cb5ca350c7392306b93c8774ebddbc
RISHABH KUMAR GHRITLAHARE <ghritlaharerishabh9@gmail.com>
2026-07-05 22:16:29 +0530

    Update 1.Data Cleaning & Preparation.py

--- 1.Data Cleaning & Preparation.py
+++ 1.Data Cleaning & Preparation.py
@@ -27,3 +27,38 @@ df_clean['Month'] = df_clean['Date'].dt.month
 print("\nData overview after cleaning:")
 print(df_clean.head())
 print("Unique Years:", df_clean['Year'].unique())
+
+
+Code output:
+
+Original Columns: ['Region', ' Date', ' Frequency', ' Estimated Unemployment Rate (%)', ' Estimated Employed', ' Estimated Labour Participation Rate (%)', 'Area']
+Cleaned Columns: ['Region', 'Date', 'Frequency', 'Estimated Unemployment Rate (%)', 'Estimated Employed', 'Estimated Labour Participation Rate (%)', 'Area']
+
+Null values before cleaning:
+Region                                     28
+Date                                       28
+Frequency                                  28
+Estimated Unemployment Rate (%)            28
+Estimated Employed                         28
+Estimated Labour Participation Rate (%)    28
+Area                                       28
+dtype: int64
+
+Null values after dropping rows:
+Region                                     0
+Date                                       0
+Frequency                                  0
+Estimated Unemployment Rate (%)            0
+Estimated Employed                         0
+Estimated Labour Participation Rate (%)    0
+Area                                       0
+dtype: int64
+
+Data overview after cleaning:
+           Region       Date Frequency  Estimated Unemployment Rate (%)  Estimated Employed  Estimated Labour Participation Rate (%)   Area  Year  Month
+0  Andhra Pradesh 2019-05-31   Monthly                             3.65          11999139.0                                    43.24  Rural  2019      5
+1  Andhra Pradesh 2019-06-30   Monthly                             3.05          11755881.0                                    42.05  Rural  2019      6
+2  Andhra Pradesh 2019-07-31   Monthly                             3.75          12086707.0                                    43.50  Rural  2019      7
+3  Andhra Pradesh 2019-08-31   Monthly                             3.32          12285693.0                                    43.97  Rural  2019      8
+4  Andhra Pradesh 2019-09-30   Monthly                             5.17          12256762.0                                    44.68  Rural  2019      9
+Unique Years: [2019 2020]
